2167c9d4a57b1d269f61beb408dbf3c0bb9ef18b
Ivan Potapenko <puivan617@gmail.com>
2023-12-26 00:28:11 +0300

    changed changeset 0001

--- src/main/resources/db/changelog/changeset/dev_0001/DML/001_insert_data_users.sql
+++ src/main/resources/db/changelog/changeset/dev_0001/DML/001_insert_data_users.sql
@@ -1,8 +1,8 @@
 --liquibase formatted sql
 --changeSet dev_0001:001-02
 
-INSERT INTO users (first_name, sur_name, middle_name, email)
-values ('Ivan', 'Ivanov', 'Ivanovich', 'ivano@mail.ru');
+INSERT INTO clients (full_name, address, payer_account_number, bank_details)
+values ('Ivan', 'Gomel', '1234567', 'bank');
 
 
 
