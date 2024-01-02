b02360c0bd9060d7ad53ac208de109b09893f08f
Pavel <pawel.grackov@gmail.com>
2024-01-02 21:52:54 +0300

    Добавил sql скрипты для создания таблицы sales и вставки в неё данных. Добавил их changelog.yaml

--- src/main/resources/db/changelog/changeset/dev_0002/DML/003_insert_data_sales.sql
+++ src/main/resources/db/changelog/changeset/dev_0002/DML/003_insert_data_sales.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
---changeSet dev_0001:003-01
+--changeSet dev_0002:003-01
 
-INSERT INTO transactions (client_id, product_id, quantity, transaction_date)
+INSERT INTO sales (client_id, product_id, quantity, sales_date)
 values ((SELECT client_id FROM clients WHERE full_name = 'Ivan' AND address = 'Gomel'),
         (SELECT product_id FROM products WHERE product_name = 'IPhone pro max'),
         1,
