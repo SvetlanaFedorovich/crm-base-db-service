b8590a08ce9448930cb4547c5b1bdbb0c4c03b1b
Furgon17 <pawel.grackov@gmail.com>
2024-01-03 20:28:26 +0300

    mvp8 change scripts insert products

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM openjdk:17-jdk-alpine3.14
-COPY ./build/libs/crm-base-db-service.jar crm-base-db-service.jar
+COPY ./build/libs/crm-base-db-service-1.0.1.jar crm-base-db-service.jar
 ENTRYPOINT ["java", "-jar", "crm-base-db-service.jar"]
--- src/main/resources/db/changelog/changeset/dev_0002/DML/003_insert_data_sales.sql
+++ src/main/resources/db/changelog/changeset/dev_0002/DML/003_insert_data_sales.sql
@@ -5,4 +5,12 @@ INSERT INTO sales (client_id, product_id, quantity, sales_date)
 values ((SELECT client_id FROM clients WHERE full_name = 'Ivan' AND address = 'Gomel'),
         (SELECT product_id FROM products WHERE product_name = 'IPhone pro max'),
         1,
-        CURRENT_DATE);
+        CURRENT_DATE),
+       ((SELECT client_id FROM clients WHERE full_name = 'Ivan' AND address = 'Gomel'),
+        (SELECT product_id FROM products WHERE product_name = 'Xiaomi'),
+        1,
+        '2023-11-11'),
+       ((SELECT client_id FROM clients WHERE full_name = 'Ivan' AND address = 'Gomel'),
+        (SELECT product_id FROM products WHERE product_name = 'MackBook'),
+        1,
+        '2023-12-26');
